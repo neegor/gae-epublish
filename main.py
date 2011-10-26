38c585fcd0d497911506389815396c32876f81da
Igor Gorschal <gorschal@gmail.com>
2011-10-26 23:31:18 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+
+class MainPage(webapp.RequestHandler):
+    
+    
+    def get(self):
+        template_values = {}
+        self.response.out.write(template.render('templates/index.html', template_values))
+
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
